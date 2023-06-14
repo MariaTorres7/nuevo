fe555041522733a66cf01640011ffcadea943c69
MariaTorres7 <maria1033097029@gmail.com>
2023-06-14 16:50:29 -0500

    add

--- js/toggle.js
+++ js/toggle.js
@@ -1,8 +1,5 @@
-
-/* 1. Asociamos el ID del toggle-box */
-let choiceBox = document.getElementById("choiceBox"); /* Circulo */
-let toggleBox = document.getElementById("toggleBox"); /* Rectangulo */
-/* Verificamos que la clase se encuentra en choice-box */
+let choiceBox = document.getElementById("choiceBox"); 
+let toggleBox = document.getElementById("toggleBox"); 
 choiceBox.addEventListener("click", function(){
   if (choiceBox.classList.contains("choiceBox")) {
     choiceBox.classList.add("choice-box-on")
